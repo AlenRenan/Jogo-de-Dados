5fa3e55ce695e9793c720c8b93288527fb148321
Renan Alencar Dores <alenrenan@gmail.com>
2021-11-15 09:18:02 -0300

    Add files via upload

---
+++ Jogo de Dados/cmdOutput.py
@@ -0,0 +1,16 @@
+def win_output():
+    print("You win :)")
+
+
+def lose_output():
+    print("You lose :(")
+
+
+def print_dice(dice1, dice2):
+    print("Dice 1:", dice1)
+    print("Dice 2:", dice2)
+
+
+
+def print_sum(sumDice):
+    print("Dice sum: ", sumDice)
