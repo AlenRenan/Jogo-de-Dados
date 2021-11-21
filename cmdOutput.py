1b644a1a546df6e73688a6d1b55fe9ff686dd8b7
Renan Alencar Dores <alenrenan@gmail.com>
2021-11-21 11:04:08 -0300

    Add files via upload

--- cmdOutput.py
+++ cmdOutput.py
@@ -6,11 +6,24 @@ def lose_output():
     print("You lose :(")
 
 
-def print_dice(dice1, dice2):
-    print("Dice 1:", dice1)
-    print("Dice 2:", dice2)
+def print_dice(dice):
+    print("\n\nDices:", dice)
 
 
+def print_sum(sum_dice):
+    print("Dice sum: ", sum_dice)
 
-def print_sum(sumDice):
-    print("Dice sum: ", sumDice)
+
+def print_tentatives(tentatives):
+    print(f"You won with: {tentatives} tentatives")
+
+
+def print_menu():
+
+    print(
+        '\n=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*\nEnter "1" roll the dices\nEnter "0" to exit\n')
+    return str(input('Choose an option: '))
+
+
+def number_of_dices():
+    return int(input("Number of Dices (1 to 5): "))
--- logic.py
+++ logic.py
@@ -1,26 +1,49 @@
-from random import randint
-import cmdOutput
+from random import sample  # imports to generate aleatory values
+import cmdOutput  # imports the cmdOutput.py
+
+"""
+Function to generate the dices
+"""
+
+n_dices = cmdOutput.number_of_dices()  # input to choose the number of dices
 
 
 def aleatory_dice():
-
-    dice1 = randint(1, 6)
-    dice2 = randint(1, 6)
-    cmdOutput.print_dice(dice1, dice2)
-    return dice1, dice2
+    dice = sample(range(1, 6), n_dices)  # generate aleatory numbers in range of 1,6
+    return dice  # return the dice
 
 
-def game_logic(dice1, dice2):
-
-    sumDide = dice1 + dice2
-    cmdOutput.print_sum(sumDide)
-
-    if sumDide == 7:
-        cmdOutput.win_output()
-    elif sumDide == 11:
-        cmdOutput.win_output()
-    else:
-        cmdOutput.lose_output()
+"""
+Function responsible for the game logic
+"""
 
 
-game_logic(*aleatory_dice())
+def game_logic(dice):
+
+    tentatives = 1  # number of tentatives
+
+    while True:  # Menu loop
+        option = cmdOutput.print_menu()  # Menu input
+
+        if option == '1':  # Roll dices option
+            dice = aleatory_dice()  # generates new aleatory numbers when the function is parameterized
+            sum_dice = sum(dice)  # Sum of dices
+            cmdOutput.print_dice(dice)  # print dice values
+            cmdOutput.print_sum(sum_dice)  # print sum
+
+            if sum_dice == 7:  # if win with sum == 7
+                cmdOutput.win_output()  # win output from cmdOutput
+                cmdOutput.print_tentatives(tentatives)  # print the number of tentatives
+                break  # stop the program
+            elif sum_dice == 11:  # if win with sum == 11
+                cmdOutput.win_output()  # win output from cmdOutput
+                cmdOutput.print_tentatives(tentatives)  # print the number of tentatives
+                break  # stop the program
+            else:
+                tentatives = tentatives + 1  # sum the number of tentatives
+                cmdOutput.lose_output()  # lose output from cmdOutput
+        elif option == "0":  # exit option
+            break  # stop the program
+
+
+game_logic(aleatory_dice())  # passes the return of aleatory_dice to the game_logic
